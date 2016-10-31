8341ebfc0cbe2f4df4444ec5e6a55a70b4eddebd
Victor Costan <costan@gmail.com>
2016-10-31 01:20:16 -0700

    Fix tests on Travis.

--- test/helpers/fixtures.rb
+++ test/helpers/fixtures.rb
@@ -1,20 +1,8 @@
 require 'fileutils'
 
-class ActiveSupport::TestCase
-  include ActiveRecord::TestFixtures
-
-  self.fixture_path = File.expand_path('../../../tmp', __FILE__)
-
-  self.use_transactional_tests = true
-  self.use_instantiated_fixtures = false
-  self.pre_loaded_fixtures = false
-  fixtures :all
-end
-
-
 # Simulate generating the fixtures.
 
-FileUtils.mkdir_p File.expand_path('../../../tmp', __FILE__)
+FileUtils.mkdir_p File.expand_path('../../../tmp/files', __FILE__)
 
 template_path = File.expand_path(
     '../../../lib/file_blobs_rails/generators/templates/file_blobs.yml.erb',
@@ -31,10 +19,23 @@ output = Erubis::Eruby.new(File.read(template_path)).result(
     blob_model: 'FileBlob' })
 File.write File.expand_path('../../../tmp/blob_owners.yml', __FILE__), output
 
-FileUtils.mkdir_p File.expand_path('../../../tmp/files', __FILE__)
 FileUtils.cp File.expand_path(
     '../../../lib/file_blobs_rails/generators/templates/files/invoice.pdf',
     __FILE__), File.expand_path('../../../tmp/files', __FILE__)
 FileUtils.cp File.expand_path(
     '../../../lib/file_blobs_rails/generators/templates/files/ruby.png',
     __FILE__), File.expand_path('../../../tmp/files', __FILE__)
+
+
+# This needs to happen after the fixture files are created.
+
+class ActiveSupport::TestCase
+  include ActiveRecord::TestFixtures
+
+  self.fixture_path = File.expand_path('../../../tmp', __FILE__)
+
+  self.use_transactional_tests = true
+  self.use_instantiated_fixtures = false
+  self.pre_loaded_fixtures = false
+  fixtures :all
+end
